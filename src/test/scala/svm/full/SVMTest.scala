8e7b693cf86ea4f74e66ef062a0459afc40d95e7
Li Haoyi <haoyi.sg@gmail.com>
2013-03-01 13:39:37 -0500

    Adding SVMTest to git

---
+++ src/test/scala/svm/full/SVMTest.scala
@@ -0,0 +1,12 @@
+package svm.full
+
+import svm.{Gen, Util}
+import org.scalatest.FreeSpec
+
+class SVMTest extends FreeSpec with Util{
+  "omg" in {
+    val tester = new Tester("svm.full.SVM")
+    println("Hello World")
+    tester.run("initSVM")
+  }
+}
