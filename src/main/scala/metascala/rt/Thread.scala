179618f3a42ef76aa6a6d29db15647368901f4b0
lihaoyi <haoyi.sg@gmail.com>
2013-08-26 23:23:50 -0700

    tightened more stuff

--- src/main/scala/metascala/rt/Thread.scala
+++ src/main/scala/metascala/rt/Thread.scala
@@ -34,20 +34,21 @@ class Thread(val threadStack: mutable.ArrayStack[Frame] = mutable.ArrayStack())(
 
 
   def doPhi(frame: Frame, oldBlock: Int, newBlock: Int) = {
-    val (srcs, dests) = frame.method.code.blocks(newBlock).phi(oldBlock).unzip
+    val phi = frame.method.code.blocks(newBlock).phi(oldBlock)
+    val (srcs, dests) = phi.unzip
     val temp = srcs.map(frame.locals)
     java.util.Arrays.fill(frame.locals, 0)
     for (i <- (0 until temp.length).optimized){
-      val (src, dest) = (temp(i), dests(i))
-      frame.locals(dest) = src
+      frame.locals(dests(i)) = temp(i)
     }
-    (srcs, dests)
+    phi
   }
 
   def jumpPhis(target: Int) = {
     doPhi(frame, frame.pc._1, target)
     frame.pc = (target, 0)
   }
+
   final def step(): Unit = try {
     //  println(frame.pc)
     val code = frame.method.code
@@ -83,7 +84,7 @@ class Thread(val threadStack: mutable.ArrayStack[Frame] = mutable.ArrayStack())(
       }else if(currentFrame.pc._1 + 1 < code.blocks.length){
         val phis = doPhi(currentFrame, currentFrame.pc._1, currentFrame.pc._1+1)
         currentFrame.pc = (currentFrame.pc._1+1, 0)
-        phis._1.zip(phis._2)
+        phis
       }else {
         Nil
       }
@@ -117,7 +118,7 @@ class Thread(val threadStack: mutable.ArrayStack[Frame] = mutable.ArrayStack())(
           throwExWithTrace("java/lang/NullPointerException", "null")
         }else{
           val phis = advancePc()
-          val ptarget = phis.toMap.getOrElse(target, target)
+          val ptarget = phis.find(_._1 == target).fold(target)(_._2)
           prepInvoke(m, args, writer(frame.locals, ptarget))
         }
 
@@ -127,7 +128,7 @@ class Thread(val threadStack: mutable.ArrayStack[Frame] = mutable.ArrayStack())(
           frame.locals.slice(s, s + t)
         }
         val phis = advancePc()
-        val ptarget = phis.toMap.getOrElse(target, target)
+
 
         val isNull = args(0) == 0
 
@@ -143,6 +144,7 @@ class Thread(val threadStack: mutable.ArrayStack[Frame] = mutable.ArrayStack())(
                 else owner.cls
               cls.vTable(mIndex)
           }
+          val ptarget = phis.find(_._1 == target).fold(target)(_._2)
           prepInvoke(mRef, args, writer(frame.locals, ptarget))
         }
 
