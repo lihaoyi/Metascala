68ee95cd7bf554738a3872c4b8d054a59a36189e
Erik LaBianca <erik.labianca@gmail.com>
2015-12-08 16:53:40 -0500

    Update stack

--- project/Build.scala
+++ project/Build.scala
@@ -1,31 +1,28 @@
+import org.scalajs.sbtplugin.cross.CrossProject
 import sbt._
 import Keys._
 
-import sbtassembly.Plugin._
-import AssemblyKeys._
+import sbtassembly.AssemblyPlugin.autoImport._
+import org.scalajs.sbtplugin.ScalaJSPlugin
+import org.scalajs.sbtplugin.ScalaJSPlugin.autoImport._
 
-object Build extends sbt.Build{
+object Build extends sbt.Build {
 
   lazy val proj = Project(
     "SVM",
     file("."),
     settings =
-      Defaults.defaultSettings ++ assemblySettings ++ Seq(
-      organization  := "com.example",
-      version       := "0.1",
-      scalaVersion  := "2.10.2",
+      Defaults.coreDefaultSettings ++ Seq(
+        organization := "com.example",
+        version := "0.1",
+        scalaVersion := "2.10.6",
 
-      resolvers ++= Seq(
-        "typesafe repo"      at "http://repo.typesafe.com/typesafe/releases/",
-        "spray nightly"      at "http://nightlies.spray.io/",
-        Resolver.sonatypeRepo("snapshots")
-      ),
-      libraryDependencies ++= Seq(
-        "org.ow2.asm" % "asm-debug-all" % "4.1",
-        "org.scalatest" % "scalatest_2.10" % "2.0" % "test",
-        "org.mozilla" % "rhino" % "1.7R4",
-        "com.nativelibs4java" % "scalaxy-loops_2.10" % "0.3-SNAPSHOT" % "provided"
+        libraryDependencies ++= Seq(
+          "org.ow2.asm" % "asm-debug-all" % "5.0.4",
+          "org.scalatest" %% "scalatest" % "2.2.5" % "test",
+          "org.mozilla" % "rhino" % "1.7R4",
+          "com.nativelibs4java" %% "scalaxy-loops" % "0.3.3" % "provided"
+        )
       )
-    )
   )
 }
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -1,9 +1,9 @@
-addSbtPlugin("io.spray" % "sbt-revolver" % "0.6.2")
+addSbtPlugin("org.scala-js" % "sbt-scalajs" % "0.6.5")
 
-addSbtPlugin("com.github.mpeltonen" % "sbt-idea" % "1.2.0")
+addSbtPlugin("io.spray" % "sbt-revolver" % "0.8.0")
 
 addSbtPlugin("me.lessis" % "less-sbt" % "0.1.10")
 
 addSbtPlugin("me.lessis" % "coffeescripted-sbt" % "0.2.3")
 
-addSbtPlugin("com.eed3si9n" % "sbt-assembly" % "0.8.5")
+addSbtPlugin("com.eed3si9n" % "sbt-assembly" % "0.14.1")
